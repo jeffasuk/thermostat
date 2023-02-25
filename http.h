23b9b2e0a5d160266ca0521e3f7be68ae9f29e20
Jeff Silver <jeff@jamcupboard.co.uk>
2023-02-25 21:02:20 +0000

    Remove obsolete file

--- http.h
+++
@@ -1,7 +0,0 @@
-/* Licensed under GNU General Public License v3.0
-  See https://github.com/jeffasuk/thermostat/blob/master/LICENSE
-  jeff at jamcupboard.co.uk
-*/
-
-extern void getRequest(WiFiClient client);
-extern void getResponse(WiFiClient client);
