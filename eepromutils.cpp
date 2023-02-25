a821f2f125634b4a386ca8a1411aaf00bdb18c3b
Jeff Silver <jeff@jamcupboard.co.uk>
2023-02-25 18:16:52 +0000

    Fix URL in header block.

--- eepromutils.cpp
+++ eepromutils.cpp
@@ -1,5 +1,5 @@
 /* Licensed under GNU General Public License v3.0
-  See https://github.com/jeffasuk/thermostat/blob/master/LICENSE
+  See https://github.com/jeffasuk/thermostat
   jeff at jamcupboard.co.uk
 */
 #include <Arduino.h>
--- eepromutils.h
+++ eepromutils.h
@@ -1,5 +1,5 @@
 /* Licensed under GNU General Public License v3.0
-  See https://github.com/jeffasuk/thermostat/blob/master/LICENSE
+  See https://github.com/jeffasuk/thermostat
   jeff at jamcupboard.co.uk
 */
 extern uint8_t eepromIsUninitialized();
